50493186e7777d40f08e1c099f4286800a9edd94
Exonorid <exonorid@gmail.com>
2021-10-15 20:40:45 -0700

    Added printer for Zig

---
+++ impls/zig/printer.zig
@@ -0,0 +1,21 @@
+
+
+//It'd be more idiomatic to add a format function to MalType,
+//I'm just doing this to match the guide
+pub fn prStr(writer: anytype, value: MalType) !void {
+    switch(value) {
+        .Nil => try writer.writeAll("nil"),
+        .List => |list| {
+            try writer.writeByte('(');
+            for(list) |elem, i| {
+                if(i > 0) try writer.writeByte(' ');
+                prStr(writer, elem);
+            }
+            try writer.writeByte(')');
+        },
+        .Int => |int| try writer.print("{d}", .{int}),
+        .Bool => |b| try writer.print("{}", .{b}),
+        .String => |str| try writer.print("\"{s}\"", .{str}),
+        .Sym => |sym| try writer.print("{s}", .{sym}),
+    }
+}
